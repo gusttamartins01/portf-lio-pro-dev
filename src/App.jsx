a2de6699d6b84c7c08f7c196f387522d003cd637
gusttamartins01 <luizgustams@gmail.com>
2026-04-05 01:09:22 -0300

    refactor(layout): reorganize app and main structure for better scalability

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
-import Home from './pages/Home'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 export default function App() {
-  return <Home />
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
